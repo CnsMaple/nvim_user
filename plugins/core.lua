8fdb02e88c7da1a8bba6228b66ea6900603d5ff8
CnsMaple <czy797@outlook.com>
2023-12-14 12:22:28 +0800

    add: 面板显示快捷键

--- plugins/core.lua
+++ plugins/core.lua
@@ -41,6 +41,19 @@ return {
       return opts
     end,
   },
+  { -- override nvim-cmp plugin
+    "hrsh7th/nvim-cmp",
+    -- override the options table that is used in the `require("cmp").setup()` call
+    opts = function(_, opts)
+      -- opts parameter is the default options table
+      -- the function is lazy loaded so cmp is able to be required
+      local cmp = require "cmp"
+      -- modify the mapping part of the table
+      opts.mapping["<C-y>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" })
+      -- return the new table to be used
+      return opts
+    end,
+  },
   -- You can disable default plugins as follows:
   -- { "max397574/better-escape.nvim", enabled = false },
   --
