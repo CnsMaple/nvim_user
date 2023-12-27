dae2b388f59eee263e7ead64fd92135762efab12
CnsMaple <czy797@outlook.com>
2023-12-27 18:08:55 +0800

    clear

--- plugins/copilot-lua-cmp.lua
+++ plugins/copilot-lua-cmp.lua
@@ -4,12 +4,8 @@ if true then
       "hrsh7th/nvim-cmp",
       dependencies = { "zbirenbaum/copilot.lua", "zbirenbaum/copilot-cmp" },
       opts = function(_, opts)
-        local cmp, copilot = require "cmp", require "copilot.suggestion"
 
         table.insert(opts.sources, { name = "copilot", priority = 1250, keyword_length = 0, group_index = 1 })
-
-        cmp.event:on("menu_opened", function() vim.b.copilot_suggestion_hidden = true end)
-        cmp.event:on("menu_closed", function() vim.b.copilot_suggestion_hidden = false end)
 
         local lspkind = require "lspkind"
         lspkind.symbol_map["Copilot"] = ""
@@ -26,44 +22,20 @@ if true then
       opts = {
         suggestion = {
           enabled = false,
-          auto_trigger = false,
-          debounce = 150,
-          keymap = {
-            accept = "<C-l>",
-            accept_word = false,
-            accept_line = false,
-            next = "<C-j>",
-            prev = "<C-k>",
-            dismiss = "<C-h>",
-          },
+        },
+        panel = {
+          enabled = false,
         },
         filetypes = {
           markdown = true,
           help = true,
-        },
-
-        panel = {
-          enabled = false,
-          auto_refresh = false,
-          keymap = {
-            jump_prev = "k",
-            jump_next = "j",
-            accept = "<CR>",
-            refresh = "<C-r>",
-            -- need in insert mode
-            open = "<M-\\>",
-          },
-          layout = {
-            position = "right", -- | top | left | right
-            ratio = 0.5,
-          },
         },
         -- copilot_node_command = "node", -- Node.js version must be > 16.x
         server_opts_overrides = {
           trace = "verbose",
           settings = {
             advanced = {
-              listCount = 3, -- #completions for panel
+              listCount = 10, -- #completions for panel
               inlineSuggestCount = 3, -- #completions for getCompletions
             },
           },
