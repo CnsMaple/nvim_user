e51b1d2929ecaee319ca350a53211f6f88d38aae
CnsMaple <czy797@outlook.com>
2023-12-27 17:32:55 +0800

    delete: tab和s-tab选中cmp

--- plugins/core.lua
+++ plugins/core.lua
@@ -29,16 +29,13 @@ return {
       return opts
     end,
   },
-  { -- override nvim-cmp plugin
+  {
     "hrsh7th/nvim-cmp",
-    -- override the options table that is used in the `require("cmp").setup()` call
     opts = function(_, opts)
-      -- opts parameter is the default options table
-      -- the function is lazy loaded so cmp is able to be required
       local cmp = require "cmp"
-      -- modify the mapping part of the table
       opts.mapping["<C-y>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" })
-      -- return the new table to be used
+      opts.mapping["<Tab>"] = nil
+      opts.mapping["<S-Tab>"] = nil
       return opts
     end,
   },
