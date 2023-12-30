1316087f98ba9daad86d62157ddf79b2fa81145b
CnsMaple <czy797@outlook.com>
2023-12-30 12:01:35 +0800

    add: 使用mark优化插件

--- plugins/user.lua
+++ plugins/user.lua
@@ -107,4 +107,19 @@ return {
     event = "User AstroFile",
     opts = {},
   },
+  {
+    "chentoast/marks.nvim",
+    event = "User AstroFile",
+    opts = {
+      excluded_filetypes = {
+        "qf",
+        "NvimTree",
+        "toggleterm",
+        "TelescopePrompt",
+        "alpha",
+        "netrw",
+        "neo-tree",
+      },
+    },
+  },
 }
