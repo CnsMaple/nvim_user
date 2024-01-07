5fed2e40f08b1a58dd25579c5df3b7aec3e1ab39
CnsMaple <czy797@outlook.com>
2024-01-08 02:22:28 +0800

    change: 更换滚动插件

--- plugins/community_scrolling_neoscroll-nvim.lua
+++
@@ -1,8 +0,0 @@
--- e7b94c5
-
-return {
-  "karb94/neoscroll.nvim",
-  event = "VeryLazy",
-  enabled = vim.g.neovide == nil,
-  opts = {},
-}
---
+++ plugins/scrolling.lua
@@ -0,0 +1,12 @@
+-- return {
+--   "karb94/neoscroll.nvim",
+--   event = "VeryLazy",
+--   enabled = vim.g.neovide == nil,
+--   opts = {},
+-- }
+return {
+  "declancm/cinnamon.nvim",
+  enabled = vim.g.neovide == nil,
+  event = "VeryLazy",
+  opts = {},
+}
